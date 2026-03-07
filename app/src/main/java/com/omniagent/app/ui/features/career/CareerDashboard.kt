0fb533374f9c5eafa3a46d2188d064e6631b675a
Satvi27-debug <satvendragirinihal@gmail.com>
2026-03-07 11:55:45 +0530

    Fixed missing imports in Career Hub features

--- app/src/main/java/com/omniagent/app/ui/features/career/CareerDashboard.kt
+++ app/src/main/java/com/omniagent/app/ui/features/career/CareerDashboard.kt
@@ -1,6 +1,7 @@
 package com.omniagent.app.ui.features.career
 
 import androidx.compose.animation.*
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
